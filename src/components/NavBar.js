c38afe00a047aa68997178b65961e465600d20db
이진실 <ljsil1204@naver.com>
2023-03-04 19:37:09 +0900

    css Moudules 적용
    1. 모듈 CSS파일 생성 및 작성
    2. className에 css이름 넣기 => 예) styles.link
    3. 여러개 클래스 이름 지정 시 배열 혹은 백틱템플릿 활용

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "@/components/NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -10,7 +11,9 @@ export default function NavBar() {
         <li>
           <Link
             href="/"
-            style={{ color: router.pathname === "/" ? "red" : "blue" }}
+            className={`${styles.link} ${
+              router.pathname === "/" ? styles.active : ""
+            }`}
           >
             Home
           </Link>
@@ -18,7 +21,10 @@ export default function NavBar() {
         <li>
           <Link
             href="/about"
-            style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+            className={[
+              styles.link,
+              router.pathname === "/about" ? styles.active : "",
+            ].join(" ")}
           >
             About
           </Link>
