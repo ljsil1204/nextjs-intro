067d21ed51557062568e649d4caa47aa3b93e4b6
이진실 <ljsil1204@naver.com>
2023-03-04 19:02:24 +0900

    리액트 훅 useState
    - react => client side rendering
    - nextjs => static pre-rendering
    - nextjs는 useState의 초기값도 같이 렌더링한다.
    - 버튼 누를  시 리액트와 상호작용?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>hellow</h1>
+      <h1>hellow {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
