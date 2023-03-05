24057badce808353d7a0de75ff62d6a702991254
이진실 <ljsil1204@naver.com>
2023-03-05 21:08:24 +0900

    Layout 컴포넌트
    - _app.js에는 컴포넌트나 스타일을 직접 넣는것은 피하는게 좋다.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,10 @@
 import "@/styles/globals.css";
-import NavBar from "@/components/NavBar";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>
-        {`
-          span {
-            color: white;
-          }
-        `}
-      </style>
-    </>
+    </Layout>
   );
 }
