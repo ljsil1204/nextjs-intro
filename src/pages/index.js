ed4a4c6594f12dde531c7ff743673ffca31a6c41
이진실 <ljsil1204@naver.com>
2023-03-05 21:51:50 +0900

    영화 api 불러오기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import Seo from "@/components/Seo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const API_KEY = "8ff489d1b740558311410e92d081a9ea";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Home {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
