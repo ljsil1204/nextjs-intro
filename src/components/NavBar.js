6f4fc9ddde6072048aa2e97073d260543c80fcbb
이진실 <ljsil1204@naver.com>
2023-03-05 21:29:57 +0900

    Image 컴포넌트
    jsx img 대신 nextjs에서 제공하는 Image 컴포넌트 사용

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -6,28 +7,42 @@ export default function NavBar() {
 
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <span className={router.pathname === "/" ? "active" : ""}>
-              Home
-            </span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <span className={router.pathname === "/about" ? "active" : ""}>
-              About
-            </span>
-          </Link>
-        </li>
-      </ul>
+      <Image src="/vercel.svg" alt="" width={80} height={30} />
+      <div>
+        <Link href="/">
+          <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+        </Link>
+        <Link href="/about">
+          <span className={router.pathname === "/about" ? "active" : ""}>
+            About
+          </span>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          background-color: red;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav span {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
