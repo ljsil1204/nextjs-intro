d950d6edecd26ee50a24239fc9a9bcd1637f94ec
이진실 <ljsil1204@naver.com>
2023-03-05 21:14:37 +0900

    Head 태그 페이지마다 적용해보기

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import NavBar from "@/components/NavBar";
+import Seo from "@/components/Seo";
 import { useState } from "react";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">Home {counter}</h1>
       <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
